dd3997f83dec3d047ca689461bd1987d3eeb79ed
ishanchamika <ishanchami9@gmail.com>
2024-09-23 17:58:25 +0530

    add delete_package function

--- src/api/ApiEndpoints.js
+++ src/api/ApiEndpoints.js
@@ -1,7 +1,9 @@
 export const SIGNIN_URL = '/api/signinuser';
 export const SIGNUP_URL = '/api/registeruser';
 export const VERIFY_USER_URL = '/api/verifyuser';
-export const ADD_PACKAGE_URL = '/api/addpackage'
+export const ADD_PACKAGE_URL = '/api/addpackage';
+export const ADMIN_DELETE_PACKAGE = '/api/deletePackage';
+
 
 export const GET_CUSTOMERS_URL = '/api/customers'
 export const ADD_CUSTOMER_URL = '/api/addcustomer'
